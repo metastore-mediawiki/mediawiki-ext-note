7f6dd92479f5092265d86e43784e4c47d9801089
Kitsune Solar <kitsune.solar@gmail.com>
2018-07-20 15:44:28 +0300

    2018-07-20 12:44:28

--- MW_EXT_Note.php
+++ MW_EXT_Note.php
@@ -7,15 +7,10 @@ if ( ! defined( 'MEDIAWIKI' ) ) {
 
 if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'MW_EXT_Note' );
-	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	// Keep i18n globals so mergeMessageFileList.php doesn't break.
 	$wgExtensionMessagesFiles['MW_EXT_Note'] = __DIR__ . '/i18n';
 
-	/* wfWarn(
-	  'Deprecated PHP entry point used for MW_EXT_Note extension. Please use wfLoadExtension instead, ' .
-	  'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
-
-	return;
+	return true;
 } else {
-	die( 'This version of the MW_EXT_Note extension requires MediaWiki 1.25+' );
+	die( 'This version of the MW_EXT_Note extension requires MediaWiki 1.30+' );
 }
